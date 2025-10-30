cf38eadf51b3c7a0b6eeb0c3f76f274cec61997c
zakzakst <e1433104@gmail.com>
2025-10-30 19:58:59 +0900

    fix

---
+++ src/components/test/button/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export const Button: React.FC<Props> = ({ children }) => {
+  return <button onClick={() => console.log('test')}>{children}</button>
+}
