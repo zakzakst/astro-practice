461a37f851f3a38439702678fce5dfcc3c2d588e
zakzakst <e1433104@gmail.com>
2025-10-31 18:43:25 +0900

    fix

--- src/components/1/section/howtoplay/index.tsx
+++ src/components/1/section/howtoplay/index.tsx
@@ -1,10 +1,6 @@
-import React from "react";
+import type React from "react";
 import styles from "./styles.module.css";
 
 export const SectionHowToPlay: React.FC = () => {
-  return (
-    <section className={styles.module} onClick={() => console.log("test")}>
-      遊び方
-    </section>
-  );
+  return <section className={styles.module}>遊び方</section>;
 };
