21b2b96fc66afc6bff1552c7066125975bc7099d
zakzakst <e1433104@gmail.com>
2025-10-31 18:35:39 +0900

    fix

--- src/components/1/section/howtoplay/index.tsx
+++ src/components/1/section/howtoplay/index.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import styles from './styles.module.css'
+import React from "react";
+import styles from "./styles.module.css";
 
 export const SectionHowToPlay: React.FC = () => {
-  return <section className={styles.module} onClick={() => console.log('test')}>遊び方</section>
-}
+  return (
+    <section className={styles.module} onClick={() => console.log("test")}>
+      遊び方
+    </section>
+  );
+};
--- src/components/test/button/index.tsx
+++ src/components/test/button/index.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const Button: React.FC<Props> = ({ children }) => {
-  return <button onClick={() => console.log('test')}>{children}</button>
-}
+  return <button onClick={() => console.log("test")}>{children}</button>;
+};
