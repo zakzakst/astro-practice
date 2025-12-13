e45e9276d0bfab3ee0ab1ebd9fff5d56af4e5241
zakzakst <e1433104@gmail.com>
2025-12-13 18:59:24 +0900

    fix

--- src/components/vue/Button.vue
+++ src/components/vue/Button.vue
@@ -1,5 +1,96 @@
+<script setup lang="ts">
+// https://github.com/joe-bell/cva/blob/main/examples/latest/vue/src/components/Button.vue
+// TODO:
+// tailwind-mergeを使うとtext-whiteがtext-oln-16B-100で上書きされる
+// tailwind-mergeを使わないと親コンポーネント側で設定したbg-amber-500よりbg-blue-900が優先される
+import { cva, type VariantProps } from "class-variance-authority";
+import { twMerge } from "tailwind-merge";
+type ButtonVariant = 'solid-fill' | 'outline' | 'text';
+type ButtonSize = 'lg' | 'md' | 'sm' | 'xs';
+
+const buttonBaseStyle = `
+  underline-offset-[calc(3/16*1rem)]
+  focus-visible:outline focus-visible:outline-4 focus-visible:outline-black focus-visible:outline-offset-[calc(2/16*1rem)] focus-visible:ring-[calc(2/16*1rem)] focus-visible:ring-yellow-300
+  aria-disabled:pointer-events-none aria-disabled:forced-colors:border-[GrayText] aria-disabled:forced-colors:text-[GrayText]
+`;
+
+const buttonVariantStyle: { [key in ButtonVariant]: string } = {
+  'solid-fill': `
+    border-4
+    border-double
+    border-transparent
+    bg-blue-900
+    text-white
+    hover:bg-blue-1000
+    hover:underline
+    active:bg-blue-1200
+    active:underline
+    aria-disabled:bg-solid-gray-300
+    aria-disabled:text-solid-gray-50
+  `,
+  outline: `
+    border
+    border-current
+    bg-white
+    text-blue-900
+    hover:bg-blue-200
+    hover:text-blue-1000
+    hover:underline
+    active:bg-blue-300
+    active:text-blue-1200
+    active:underline
+    aria-disabled:bg-white
+    aria-disabled:text-solid-gray-300
+  `,
+  text: `
+    text-blue-900
+    underline
+    hover:bg-blue-50
+    hover:text-blue-1000
+    hover:decoration-[calc(3/16*1rem)]
+    active:bg-blue-100
+    active:text-blue-1200
+    focus-visible:bg-yellow-300
+    aria-disabled:bg-transparent
+    aria-disabled:focus-visible:bg-yellow-300
+    aria-disabled:text-solid-gray-300
+  `,
+};
+
+const buttonSizeStyle: { [key in ButtonSize]: string } = {
+  lg: 'min-w-[calc(136/16*1rem)] min-h-14 rounded-8 px-4 py-3 text-oln-16B-100',
+  md: 'min-w-24 min-h-12 rounded-8 px-4 py-2 text-oln-16B-100',
+  sm: 'relative min-w-20 min-h-9 rounded-6 px-3 py-0.5 text-oln-16B-100 after:absolute after:inset-x-0 after:-inset-y-full after:m-auto after:h-[44px]',
+  xs: 'relative min-w-18 min-h-7 rounded-4 px-2 py-0.5 text-oln-14B-100 after:absolute after:inset-x-0 after:-inset-y-full after:m-auto after:h-[44px]',
+};
+
+const buttonStyle = cva(buttonBaseStyle, {
+  variants: {
+    variant: buttonVariantStyle,
+    size: buttonSizeStyle,
+  },
+})
+
+type Props = {
+  size?: ButtonSize;
+  variant?: ButtonVariant;
+  className?: string;
+};
+
+const { size = 'md', variant = 'solid-fill' } = defineProps<Props>();
+</script>
+
 <template>
-  <button>
+  <!-- <button :class="[
+    buttonStyle({ size, variant }),
+    className,
+  ]">
+    <slot />
+  </button> -->
+  <button :class="twMerge(
+    buttonStyle({ size, variant }),
+    className,
+  )">
     <slot />
   </button>
 </template>
