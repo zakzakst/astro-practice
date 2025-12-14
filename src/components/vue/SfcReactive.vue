5c55d2b18f63fcbaf0cf28ec91c5dc3de896f537
zakzakst <e1433104@gmail.com>
2025-12-14 14:25:25 +0900

    fix

---
+++ src/components/vue/SfcReactive.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+  import { reactive, computed } from 'vue'
+  const widthInit = Math.round(Math.random() * 10)
+  const heightInit = Math.round(Math.random() * 10)
+  const rectangle = reactive({
+    width: widthInit,
+    height: heightInit,
+  })
+  const area = computed(
+    (): number => {
+      return rectangle.width * rectangle.height
+    }
+  )
+  setInterval(
+    (): void => {
+      rectangle.width = Math.round(Math.random() * 10)
+      rectangle.height = Math.round(Math.random() * 10)
+    },
+    1000
+  )
+</script>
+
+<template>
+  <p>縦{{ rectangle.height }}で横が{{ rectangle.width }}の長方形の面積は{{ area }}</p>
+</template>
